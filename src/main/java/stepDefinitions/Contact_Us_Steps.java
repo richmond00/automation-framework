5561856b70d3dcaff096838179dce04142950726
Dongwook Kim <dongwook227@gmail.com>
2023-06-03 17:28:09 -0400

    added hooks for before/after

--- src/main/java/stepDefinitions/Contact_Us_Steps.java
+++ src/main/java/stepDefinitions/Contact_Us_Steps.java
@@ -1,11 +1,32 @@
 package stepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Contact_Us_Steps {
+    private WebDriver driver;
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
     @Given("I access the webdriver university")
     public void i_access_the_webdriver_university() {
         System.out.println("Test1");
